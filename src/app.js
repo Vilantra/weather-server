4103a3003d10c993193562ae9f19e6cbfdec21e1
vilantra <mua.amelie@gmail.com>
2020-06-24 16:24:51 -0400

    setup for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast');
 
 const app = express()
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -95,17 +96,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}.`)
 })
-
-/*
-Goal: Wire up /weather
-1. Requiere geocode/forecast into app.js
-2. Use the address to geocode
-3. Use the coordinates to get forecast
-4. Send back the real forecast and location
-
-
-
-*/
